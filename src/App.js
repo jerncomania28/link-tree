d8a1000dd12dc1fe015ae84989ec6efeaed25adb
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-10-25 14:39:36 +0000

    share button and hover effect added !

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import { Link } from "react-router-dom";
 import "./App.css";
 
 // asset
 
 import ProfileImg from "./assets/instagram-4.jpeg";
+import ShareBtn from "./assets/share-btn.svg";
 
-const LinkTemplate = ({ linkValue, id, externalLink }) => {
+const LinkTemplate = ({ data }) => {
+  const { linkValue, id, externalLink } = data;
   return (
-    <Link
-      to={{ pathname: `${externalLink}` }}
+    <a
+      href={externalLink}
       target="_blank"
-      rel="noopener"
+      rel="noopener noreferrer"
       id={id}
       className="link"
     >
       {linkValue}
-    </Link>
+    </a>
   );
 };
 
 const App = () => {
+  const Links = {
+    0: {
+      linkValue: "Twitter Link",
+      id: "twitter",
+      externalLink: "https://twitter.com/tech_optician",
+    },
+    1: {
+      linkValue: "Zuri Team",
+      id: "btn_zuri",
+      externalLink: "https://training.zuri.team/",
+    },
+    2: {
+      linkValue: "Zuri Books",
+      id: "books",
+      externalLink: "https://books.zuri.team",
+    },
+    3: {
+      linkValue: "Python Books",
+      id: "book_python",
+      externalLink: "https://books.zuri.team/python-for-beginners?ref_id=jay",
+    },
+    4: {
+      linkValue: "Background Check For Coders",
+      id: "pitch",
+      externalLink: "https://background.zuri.team",
+    },
+    5: {
+      linkValue: "Design Books",
+      id: "book_design",
+      externalLink: "https://books.zuri.team/design-rules",
+    },
+  };
+
   return (
     <div className="app">
+      
+      {/* share button */}
+      <div className="app-share">
+        <img src={ShareBtn} alt="share-btn" />
+      </div>
 
       {/* profile container */}
       <div className="app-profile">
-
         {/* profile display image */}
         <div className="app-profile--img">
           <img src={ProfileImg} alt="profile-avatar" />
@@ -38,10 +76,10 @@ const App = () => {
         </div>
       </div>
 
-
-      <LinkTemplate linkValue="test" id="test" externalLink="https://twitter.com/" />
-
-
+      {/* links */}
+      {Object.keys(Links).map((_idx) => (
+        <LinkTemplate data={Links[_idx]} />
+      ))}
     </div>
   );
 };
