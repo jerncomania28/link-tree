f522f30e14a19f3fed9a275c5b4a2955ec43367d
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-11-04 12:26:29 +0000

    style:added most of the styles for the contact form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -6,10 +6,72 @@ import Footer from "../../components/Footer/Footer"
 import "./ContactPage.css";
 
 
+const InputField = ({ label, type, placeholder, name, className }) => {
+    return (
+        <div className={className}>
+            <label>{label}</label> <br />
+            <input type={type} placeholder={placeholder} name={name} /> <br />
+            <small className="error-message"> Dislays error messages</small>
+        </div>
+
+    )
+
+}
+
 const ContactPage = () => {
     return (
-        <div>
-            this is the contact page .
+        <div className="contact">
+            <div className="contact-container">
+                <div className="contact-header">
+                    <h3>Contact Me</h3>
+                    <p>Hi there , Contact me to ask me about anything you have in mind.</p>
+                </div>
+
+
+                <form className="contact-form">
+
+                    <div className="contact-form--container">
+                        <InputField
+                            label="first name"
+                            type="text"
+                            placeholder="Enter your first name"
+                            name='firstName'
+                            className="contact-form--container__input"
+                        />
+                        <InputField
+                            label="last name"
+                            type="text"
+                            placeholder="Enter your last name"
+                            name='lastName'
+                            className="contact-form--container__input"
+                        />
+                    </div>
+
+                    <InputField
+                        label="Email"
+                        type="email"
+                        placeholder="yourname@email.com"
+                        name="email"
+                        className="contact-form--container__wide"
+                    />
+
+
+                    <div className="contact-form--comment">
+                        <label>Message</label>
+                        <textarea
+                            placeholder="send me a message and I'll reply you as soon as possible."
+                        ></textarea>
+                    </div>
+
+                    <div className="contact-form--checkbox">
+                        <input type="checkbox" /> <span>You agree to providing your data to Jeremiah who May contact you.</span>
+                    </div>
+
+                    <button className="contact-form-button">send message</button>
+
+
+                </form>
+            </div>
 
             <Footer />
         </div>
