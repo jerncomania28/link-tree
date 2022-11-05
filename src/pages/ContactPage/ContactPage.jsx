3d89e1b2dab8a33720c4e39335d87111e9e47d01
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-11-05 13:02:29 +0000

    feat:finished all the required functionality!

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,24 +1,65 @@
 
 // components 
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer"
 
 // styles 
 import "./ContactPage.css";
 
 
-const InputField = ({ label, type, placeholder, name, className }) => {
+const InputField = ({ label, type, placeholder, name, className, id, handleChange, showError, formInput }) => {
     return (
         <div className={`input-field ${className}`}>
-            <label>{label}</label> <br />
-            <input type={type} placeholder={placeholder} name={name} /> <br />
-            {/* <small className="error-message"> Dislays error messages</small> */}
+            <label htmlFor={`#${id}`}>{label}</label> <br />
+            <input
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                id={id}
+                onChange={handleChange}
+                value={formInput[name]}
+            />
+            <br />
+            {
+                (showError && formInput[name].length === 0) &&
+                <small className="error-message">{`${type} field cannot be Empty!`}</small>
+            }
         </div>
-
     )
-
 }
 
 const ContactPage = () => {
+
+    const defaultForm = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        message: ""
+    }
+
+    const [formInput, setFormInput] = useState(defaultForm);
+    const [showError, setShowError] = useState(false);
+
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormInput({ ...formInput, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!Object.values(formInput).every(Boolean)) {
+            setShowError(true);
+        } else {
+            setShowError(false);
+            alert("response recorded!")
+            setFormInput(defaultForm)
+        }
+
+    }
+
     return (
         <div className="contact">
             <div className="contact-container">
@@ -37,6 +78,10 @@ const ContactPage = () => {
                             placeholder="Enter your first name"
                             name='firstName'
                             className="contact-form--container__input"
+                            id="first_name"
+                            handleChange={handleChange}
+                            showError={showError}
+                            formInput={formInput}
                         />
                         <InputField
                             label="last name"
@@ -44,6 +89,10 @@ const ContactPage = () => {
                             placeholder="Enter your last name"
                             name='lastName'
                             className="contact-form--container__input"
+                            id="last_name"
+                            handleChange={handleChange}
+                            showError={showError}
+                            formInput={formInput}
                         />
                     </div>
 
@@ -53,22 +102,40 @@ const ContactPage = () => {
                         placeholder="yourname@email.com"
                         name="email"
                         className="contact-form--container__wide"
+                        id="email"
+                        handleChange={handleChange}
+                        showError={showError}
+                        formInput={formInput}
                     />
 
 
                     <div className="contact-form--comment">
-                        <label>Message</label>
+                        <label htmlFor="#message">Message</label>
                         <textarea
+                            id="message"
                             placeholder="send me a message and i'll reply you as soon as possible."
+                            name="message"
+                            onChange={handleChange}
+                            value={formInput["message"]}
                         ></textarea>
+                        {
+                            (showError && formInput["message"].length === 0) &&
+                            <small className="error-message"> please enter a message </small>
+                        }
                     </div>
 
                     <div className="contact-form--checkbox">
                         <input type="checkbox" /> <span>You agree to providing your data to Jeremiah who May contact you.</span>
                     </div>
 
-                    <button className="contact-form-button">send message</button>
-
+                    <button
+                        type="submit"
+                        className="contact-form-button"
+                        id="btn__submit"
+                        onClick={handleSubmit}
+                    >
+                        send message
+                    </button>
 
                 </form>
             </div>
